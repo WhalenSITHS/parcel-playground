c83336959f7d5b009d38d2c8507a4ab40a3b822e
Michael Whalen <michael.whalen@sitechhs.com>
2020-03-04 20:59:40 -0500

    effect works, some media queries added

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import AOS from "aos";
 import anime from "animejs/lib/anime.es.js";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
-
+import hoverEffect from "hover-effect";
 AOS.init();
 const checkBox = document.querySelector(".navigation_checkbox");
 const links = document.querySelectorAll(".navigation_link");
@@ -31,5 +31,13 @@ let fill = anime({
   loop: true
 });
 
-/* stroke(); */
-fill();
+new hoverEffect({
+  parent: document.querySelector(".transition-image"),
+  intensity1: 0.1,
+  intensity2: 0.1,
+  angle2: Math.PI / 2,
+  image1: require("../media/trans-1.jpg"),
+  image2: require("../media/trans-2.jpg"),
+  displacementImage:
+    "https://cdn.rawgit.com/robin-dela/hover-effect/b6c6fd26/images/stripe1mul.png?raw=true"
+});
