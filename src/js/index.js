d8a508b813e3dd5bb55472a39fd9a2e1e5d45bc6
Michael Whalen <michael.whalen@sitechhs.com>
2020-03-07 11:43:27 -0500

    need further testing in seperate enviornment

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,12 @@
 import AOS from "aos";
 import anime from "animejs/lib/anime.es.js";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
 import hoverEffect from "hover-effect";
+
 AOS.init();
 const checkBox = document.querySelector(".navigation_checkbox");
 const links = document.querySelectorAll(".navigation_link");
@@ -13,6 +17,7 @@ linkarr.forEach(el =>
   })
 );
 
+///AOS Animation on SVG
 let stroke = anime({
   targets: "#svg-header path",
   strokeDashoffset: [anime.setDashoffset, 0],
@@ -31,6 +36,7 @@ let fill = anime({
   loop: true
 });
 
+///////Hover Effect
 new hoverEffect({
   parent: document.querySelector(".transition-image"),
   intensity1: 0.1,
@@ -41,3 +47,33 @@ new hoverEffect({
   displacementImage:
     "https://cdn.rawgit.com/robin-dela/hover-effect/b6c6fd26/images/stripe1mul.png?raw=true"
 });
+
+//////3-d Model scene
+
+function initModel() {
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xdddddd);
+  let camera = new THREE.PerspectiveCamera(
+    40,
+    window.innerWidth / window.innerHeight,
+    1,
+    5000
+  );
+  camera.rotation.y = (45 / 180) * Math.PI;
+  camera.position.x = 800;
+  camera.position.y = 100;
+  camera.position.z = 1000;
+  let renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.querySelector(".model").appendChild(renderer.domElement);
+  ////requiers gltfloader
+  let loader = new GLTFLoader();
+  loader.load("scene.gltf", function(gltf) {
+    let headphones = gltf.scene.children[0];
+    headphones.scale.set(0.5, 0.5, 0.5);
+    scene.add(gltf.scene);
+    //animate();
+  });
+}
+
+initModel();
